5fe8647d1d555063a384608f5afd933ddcb3d2c4
Daler Asrorov <asrorids@gmail.com>
2020-02-18 03:01:40 -0500

    twelve days of christmas lyrics

--- ch1/src/main.rs
+++ ch1/src/main.rs
@@ -1,7 +1,9 @@
 // mod temp_converter;
-mod fibonacci_number;
+// mod fibonacci_number;
+mod twelve_days_of_christmas_lyrics;
 
 fn main() {
     // temp_converter::start_program();
-    fibonacci_number::start_program();
+    // fibonacci_number::start_program();
+    twelve_days_of_christmas_lyrics::start_program();
 }
---
+++ ch1/src/twelve_days_of_christmas_lyrics.rs
@@ -0,0 +1,45 @@
+
+pub fn start_program() {
+  display_song_lyrics(12);
+}
+
+fn initialize_lyrics_lines() -> [String; 12] {
+  let array: [String; 12] = [
+    "A partridge in a pear tree".to_string(),
+    "turtle doves".to_string(),
+    "french hens".to_string(),
+    "calling birds".to_string(),
+    "golden rings".to_string(),
+    "geese a-laying".to_string(),
+    "swans a-swimming".to_string(),
+    "maids a-milking".to_string(),
+    "ladies dancing".to_string(),
+    "lords a-leaping".to_string(),
+    "pipers piping".to_string(),
+    "drummers drumming".to_string()
+  ];
+
+  return array;
+}
+
+fn starting_lyric(num: i32) -> String {
+  return ["On the", &num.to_string(), "day of Christmas my true love sent to me"].join(" ")
+}
+
+fn display_song_lyrics(num: i32) {
+  let lyrics_array: [String; 12] = initialize_lyrics_lines();
+
+  for i in 1..num + 1 {
+    let start = starting_lyric(i);
+    println!("\t{}", start);
+
+    for j in (0..i).rev() {
+      match j {
+        0 => println!("\t{}", lyrics_array[j as usize]),
+        _ => println!("\t{} {}", j + 1, lyrics_array[j as usize]),
+      }
+    }
+
+    println!("\n");
+  }
+}
