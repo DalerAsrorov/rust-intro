552b244c1601454a7573a2a8903f3d7d4ddb0218
Daler Asrorov <asrorids@gmail.com>
2020-02-27 00:38:07 -0500

    some enum examples

--- ch6/src/main.rs
+++ ch6/src/main.rs
@@ -4,10 +4,45 @@ enum IpAddr {
     V6(String),
 }
 
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
+
 fn main() {
     let home = IpAddr::V4(String::from("127.0.0.1"));
     let loopback = IpAddr::V6(String::from("::1"));
 
     println!("{:#?}", home);
     println!("{:#?}", loopback);
+
+    println!("Coin value for {:?}: {}", Coin::Penny, value_in_cents(Coin::Penny));
+    println!("Coin value for {:?}: {}", Coin::Nickel, value_in_cents(Coin::Nickel));
+    println!("Coin value for {:?}: {}", Coin::Dime, value_in_cents(Coin::Dime));
+    println!("Coin value for {:?}: {}", Coin::Quarter(UsState::Alaska), value_in_cents(Coin::Quarter(UsState::Alaska)));
 }
