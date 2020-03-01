36e35f66825f0e33dd21f29b2f9b2816f9dbb34b
Daler Asrorov <asrorids@gmail.com>
2020-03-01 02:36:49 -0500

    useage of format and string concat using '+'

--- ch8/src/main.rs
+++ ch8/src/main.rs
@@ -3,6 +3,7 @@ mod spreadhseet_cell;
 use spreadhseet_cell::SpreadsheetCell;
 
 fn main() {
+    // --- Vectors Overview ---
     let mut v = vec![1, 2, 3, 4, 5];
     let third: &i32 = &v[2];
 
@@ -23,7 +24,7 @@ fn main() {
         *i += 50;
     }
 
-    let row = vec![
+    let mut row = vec![
         SpreadsheetCell::Int(3),
         SpreadsheetCell::Text(String::from("blue")),
         SpreadsheetCell::Float(10.12)
@@ -32,4 +33,35 @@ fn main() {
     for el in &row {
         println!("element: {:?}", el);
     }
+
+    let last_el = row.pop();
+
+    println!("last element from row: {:?}", last_el);
+
+    // --- Strings Overview ---
+
+    let mut new_s = String::new();
+    new_s.push('f');
+    new_s.push_str("bar");
+
+    let mut new_s1 = String::from(&new_s);
+
+    new_s1.push_str(&new_s);
+
+    println!("new_s1='{}'", new_s1);
+
+    let mut s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    s1 = s1 + &s2; // note s1 has been moved here and can no longer be used
+
+    println!("{}, {}", s1, s2);
+
+    let ss1 = String::from("tic");
+    let ss2 = String::from("tac");
+    let ss3 = String::from("toe");
+
+    // the format macro doesn't take ownership of its params
+    let ss = format!("{}-{}-{}", ss1, ss2, ss3);
+
+    println!("{}", ss);
 }
